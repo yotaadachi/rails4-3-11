3a59b35b35727806438f046ff6d3e762239ad8dd
安達陽太 <yota@MacBook-Pro.local>
2020-06-21 15:01:12 +0900

    Finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '15f44d82fb95375e08bf32ef9a3dd1e84c3788f91a1a31d708f1a39c274adbf08b3c17f81eee906afe7b90c940446232ea7d7e14be503e7b84da8ce5e1004bb9'
---
+++ db/migrate/20200621050431_add_image_idto_list.rb
@@ -0,0 +1,4 @@
+class AddImageIdtoList < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
